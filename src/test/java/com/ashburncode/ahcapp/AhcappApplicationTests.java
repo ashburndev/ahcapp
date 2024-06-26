4c21ef545f1bf83ed0547f58fc8c62d7f51810a5
ashburndev <ashburndev@gmail.com>
2024-06-26 08:04:11 -0400

    initial commit

---
+++ src/test/java/com/ashburncode/ahcapp/AhcappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashburncode.ahcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
